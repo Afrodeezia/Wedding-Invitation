58aec781411eb1635b6cfcaed9993e95bf9a73e2
Afrodeezia <marcopauloangeloroldan1@gmail.com>
2022-03-16 22:19:28 +0800

    fixed burger menu, overlay and scroll to top button, will work on finalizing the design and responsiveness

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 //------------------ OVERLAY ------------------------------
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-    
-  }
+  $(".menu-icon").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay-menu").toggleClass("open");
+  });
 
   function homeCloseNav() {
     window.scrollTo(0,0);
@@ -31,41 +27,11 @@ function closeNav() {
     document.getElementById("myNav").style.width = "0%";
     
   }
-//------------------- OVERLAY ------------------------------------
 
-
- //----------------- NAVBAR STICKY -------------------------------
-  // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction(); scrollFunction();};
-
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. 
-//Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= 100) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-} 
-//----------------- NAVBAR STICKY -------------------------------
-
-function scrollWin(){
-  window.scrollTo(0,500);
-}
-
-
-
-//----------------- NAVIGATION BUTTONS --------------------------
 
 //----------------- SCROLL TOP TOP BUTTON -----------------------
 
-
+window.onscroll = function() {scrollFunction();};
 
 function scrollFunction() {
   if(document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200){
@@ -81,9 +47,5 @@ function topFunction() {
   document.documentElement.scrollTop = 0; 
 }
 
-// ----------- burger button and overlay -----------------
 
-$(".menu-icon").click(function() {
-  $(this).toggleClass("active");
-  $(".overlay-menu").toggleClass("open");
-});
+
